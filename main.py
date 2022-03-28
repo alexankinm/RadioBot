62b0e3265b5ac4a59eaf7a67e00088b28d464be2
Esty88 <96884690+alexankinm@users.noreply.github.com>
2022-03-28 13:26:27 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from discord import FFmpegPCMAudio, Activity, ActivityType
+from discord.ext.commands import Bot
+import os
+import keep_alive
+bot = Bot(command_prefix="-", help_command=None)
+
+@bot.event
+async def on_ready() -> None:
+    print(f"Бот {bot.user} успешно запущен.")
+    await bot.change_presence(activity=Activity(name="музончик :3",  type=ActivityType.listening)) #статус бота (Слушает ... (действие в кавычках))
+
+    voice_channel = bot.get_channel(940190057556488222) #айди голосового канала
+    player = await voice_channel.connect()
+    player.play(FFmpegPCMAudio("http://s02.fjperezdj.com:8006/live")) #ссылка на радио. Указывать в кавычках.
+    
+keep_alive.keep_alive()
+bot.run(os.environ["OTU3OTUwMTkxNzUzOTEyMzMw.YkGOow.BoR94jCU9kAw9W9NhgJviRo8tT4"])
