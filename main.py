b164705654366e3e105f563b6dc77edd23bc938b
Esty88 <96884690+alexankinm@users.noreply.github.com>
2022-05-09 09:20:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ async def on_ready() -> None:
     print(f"Бот {bot.user} успешно запущен.")
     await bot.change_presence(activity=Activity(name="музончик :3",  type=ActivityType.listening)) #статус бота (Слушает ... (действие в кавычках))
 
-    voice_channel = bot.get_channel(940190057556488222) #айди голосового канала
+    voice_channel = bot.get_channel(VOICE ID) #айди голосового канала
     player = await voice_channel.connect()
-    player.play(FFmpegPCMAudio("http://s02.fjperezdj.com:8006/live")) #ссылка на радио. Указывать в кавычках.
+    player.play(FFmpegPCMAudio("http://s02.fjperezdj.com:8006/live")) #ссылка на радио
     
 keep_alive.keep_alive()
 bot.run("TOKEN")
